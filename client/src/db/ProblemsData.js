faeabb3a7874cbc076674dac2a9e5a27fbccb902
PrateekJha14 <139892883+PrateekJha14@users.noreply.github.com>
2025-04-09 05:54:42 +0530

    added problems

--- client/src/db/ProblemsData.js
+++ client/src/db/ProblemsData.js
@@ -2,47 +2,262 @@ const problemsData = {
     "Binary Search": {
       easy: [
         {
-          name: "Less or Equal",
-          link: "https://codeforces.com/contest/1594/problem/C",
-          contestId: 1594,
-          index: "C"
-        },
-        {
-          name: "Sagheer and Nubian Market",
-          link: "https://codeforces.com/contest/1221/problem/C",
-          contestId: 1221,
-          index: "C"
-        }
-      ],
-      medium: [
-        {
-          name: "Zero Array",
-          link: "https://codeforces.com/problemset/problem/1201/B",
-          contestId: 1201,
+          name: "Buying Lemonade",
+          link: "https://codeforces.com/problemset/problem/2024/B",
+          contestId: 2024,
           index: "B"
         },
         {
-          name: "Military Problem",
-          link: "https://codeforces.com/problemset/problem/1006/E",
-          contestId: 1006,
-          index: "E"
-        }
-      ],
-      hard: [
+          name: "Robin Hood in Town",
+          link: "https://codeforces.com/problemset/problem/2014/C",
+          contestId: 2014,
+          index: "C"
+        },
         {
-          name: "Flood Fill",
-          link: "https://codeforces.com/problemset/problem/1114/D",
-          contestId: 1114,
+          name: "Building an Aquarium",
+          link: "https://codeforces.com/problemset/problem/1873/E",
+          contestId: 1873,
+          index: "E"
+        },
+        {
+          name: "Traffic Light",
+          link: "https://codeforces.com/problemset/problem/1744/C",
+          contestId: 1744,
+          index: "C"
+        },
+        {
+          name: "Save More Mice",
+          link: "https://codeforces.com/problemset/problem/1593/C",
+          contestId: 1593,
+          index: "C"
+        }
+        
+      ],
+      medium: [
+        {
+          name: "Black Cells",
+          link: "https://codeforces.com/problemset/problem/2026/B",
+          contestId: 2026,
+          index: "B"
+        },
+        {
+          name: "New Game",
+          link: "https://codeforces.com/problemset/problem/2025/C",
+          contestId: 2025,
+          index: "C"
+        },
+        {
+          name: "Brightness Begins",
+          link: "https://codeforces.com/problemset/problem/2020/B",
+          contestId: 2020,
+          index: "B"
+        },
+        {
+          name: "Boring Day",
+          link: "https://codeforces.com/problemset/problem/1982/C",
+          contestId: 1982,
+          index: "C"
+        },
+        {
+          name: "Chamo and Mocha's Array",
+          link: "https://codeforces.com/problemset/problem/1975/C",
+          contestId: 1975,
+          index: "C"
+        },
+        {
+          name: "Battle Cows",
+          link: "https://codeforces.com/problemset/problem/1951/B",
+          contestId: 1951,
+          index: "B"
+        },
+        {
+          name: "Outstanding Impressionist",
+          link: "https://codeforces.com/problemset/problem/2053/B",
+          contestId: 2053,
+          index: "B"
+        },
+        {
+          name: "Place of the Olympiad",
+          link: "https://codeforces.com/problemset/problem/2091/D",
+          contestId: 2091,
           index: "D"
         },
         {
-          name: "Covered Points Count",
-          link: "https://codeforces.com/problemset/problem/1000/C",
-          contestId: 1000,
+          name: "Insane Problem",
+          link: "https://codeforces.com/problemset/problem/2044/E",
+          contestId: 2044,
+          index: "E"
+        },
+        {
+          name: "Counting Pairs",
+          link: "https://codeforces.com/problemset/problem/2051/D",
+          contestId: 2051,
+          index: "D"
+        },
+      ],
+      hard: [
+        {
+          name: "Two Colors",
+          link: "https://codeforces.com/problemset/problem/2075/C",
+          contestId: 2075,
           index: "C"
+        },
+        {
+          name: "Limited Repainting",
+          link: "https://codeforces.com/problemset/problem/2070/C",
+          contestId: 2070,
+          index: "C"
+        },
+        {
+          name: "Trinity",
+          link: "https://codeforces.com/problemset/problem/2032/C",
+          contestId: 2032,
+          index: "C"
+        },
+        {
+          name: "Alice's Adventures in Permuting",
+          link: "https://codeforces.com/problemset/problem/2028/B",
+          contestId: 2028,
+          index: "B"
+        },
+        {
+          name: "Action Figures",
+          link: "https://codeforces.com/problemset/problem/2026/C",
+          contestId: 2026,
+          index: "C"
+        },
+      ]
+    },
+    "Two Pointers & Sliding Window": {
+      easy: [
+        {
+          name: "Rakhsh's Revival",
+          link: "https://codeforces.com/problemset/problem/2034/B",
+          contestId: 2034,
+          index: "B"
+        },
+        {
+          name: "Make Three Regions",
+          link: "https://codeforces.com/problemset/problem/1997/B",
+          contestId: 1997,
+          index: "B"
+        },
+        {
+          name: "Very Different Array",
+          link: "https://codeforces.com/problemset/problem/1921/D",
+          contestId: 1921,
+          index: "D"
+        },
+        {
+          name: "Yarik and Array",
+          link: "https://codeforces.com/problemset/problem/1899/C",
+          contestId: 1899,
+          index: "C"
+        },
+        {
+          name: "Haunted House",
+          link: "https://codeforces.com/problemset/problem/1884/B",
+          contestId: 1884,
+          index: "B"
         }
+        
+      ],
+      medium: [
+        {
+          name: "Sharky Surfing",
+          link: "https://codeforces.com/problemset/problem/2037/D",
+          contestId: 2037,
+          index: "D"
+        },
+        {
+          name: "Right Left Wrong",
+          link: "https://codeforces.com/problemset/problem/2000/D",
+          contestId: 2000,
+          index: "D"
+        },
+        {
+          name: "Beppa and SwerChat",
+          link: "https://codeforces.com/problemset/problem/1776/H",
+          contestId: 1776,
+          index: "H"
+        },
+        {
+          name: "Longest Strike",
+          link: "https://codeforces.com/problemset/problem/1676/F",
+          contestId: 1676,
+          index: "F"
+        },
+        {
+          name: "Chamo and Mocha's Array",
+          link: "https://codeforces.com/problemset/problem/1975/C",
+          contestId: 1975,
+          index: "C"
+        },
+        {
+          name: "Boats Competition",
+          link: "https://codeforces.com/problemset/problem/1399/C",
+          contestId: 1399,
+          index: "C"
+        },
+        {
+          name: "Most socially-distanced subsequence",
+          link: "https://codeforces.com/problemset/problem/1364/B",
+          contestId: 1364,
+          index: "B"
+        },
+        {
+          name: "XXXXX",
+          link: "https://codeforces.com/problemset/problem/1364/A",
+          contestId: 1364,
+          index: "A"
+        },
+        {
+          name: "Money Trees",
+          link: "https://codeforces.com/problemset/problem/1873/F",
+          contestId: 1873,
+          index: "F"
+        },
+        {
+          name: "Scoring Subsequences",
+          link: "https://codeforces.com/problemset/problem/1794/C",
+          contestId: 1794,
+          index: "C"
+        },
+      ],
+      hard: [
+        {
+          name: "Counting Points",
+          link: "https://codeforces.com/problemset/problem/2074/D",
+          contestId: 2074,
+          index: "D"
+        },
+        {
+          name: "The Trail",
+          link: "https://codeforces.com/problemset/problem/2055/C",
+          contestId: 2055,
+          index: "C"
+        },
+        {
+          name: "Inaccurate Subsequence Search",
+          link: "https://codeforces.com/problemset/problem/1955/D",
+          contestId: 1955,
+          index: "D"
+        },
+        {
+          name: "LR-remainders",
+          link: "https://codeforces.com/problemset/problem/1932/C",
+          contestId: 1932,
+          index: "C"
+        },
+        {
+          name: "Smilo and Monsters",
+          link: "https://codeforces.com/problemset/problem/1891/C",
+          contestId: 1891,
+          index: "C"
+        },
       ]
     }
+
   };
   
   export default problemsData;
